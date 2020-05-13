ebf4162770ffa8d198a6767f5bfa8cb74bbcb8a9
lexieAA <43833670+lexieAA@users.noreply.github.com>
2020-05-13 14:34:30 -0700

    library paths added

--- src/main/java/com/ss/lms/orchestrator/controller/SecurityControlledAPIs.java
+++ src/main/java/com/ss/lms/orchestrator/controller/SecurityControlledAPIs.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,15 +32,47 @@ public class SecurityControlledAPIs {
     }
     
     //------------Requests for library-----------------------
+    //read all branches
     @GetMapping(path="/lms/public/library/branches")
-	public ResponseEntity<String> branch() throws SQLException {
+	public ResponseEntity<String> branch1() throws SQLException {
 		return restTemplate.getForEntity("http://LIBRARY-SERVICE/lms/public/library/branches/", String.class);
 	}
     
-    @GetMapping(path="/lms/public/library/branches/{branchId}}")
-	public ResponseEntity<String> branch(@PathVariable int branchId) throws SQLException {
-		return restTemplate.getForEntity("http://LIBRARY-SERVICE/lms/public/library/branches/"+branchId, String.class);
+    //read by branch Id
+    @GetMapping("/lms/public/library/branches/branch/{branchId}")
+	public ResponseEntity<String> branch2(@PathVariable Long branchId) throws SQLException {
+		return restTemplate.getForEntity("http://LIBRARY-SERVICE/lms/public/library/branches/branch/"+branchId, String.class);
 	}
     
-    //To do for all paths
+    // update all branches
+ 	@PutMapping("/lms/public/library/branches/branch/{branchId}")
+ 	public ResponseEntity<String> branch3(@PathVariable Long branchId) throws SQLException {
+		return restTemplate.getForEntity("http://LIBRARY-SERVICE/lms/public/library/branches/branch/"+branchId, String.class);
+	}
+    
+ 	// read all book copies by branchId
+ 	@GetMapping("/lms/public/library/branches/branch/{branchId}/bookCopies")
+ 	public ResponseEntity<String> copies(@PathVariable Long branchId) throws SQLException {
+		return restTemplate.getForEntity("http://LIBRARY-SERVICE/lms/public/library/branches/branch/"+branchId+"/bookCopies", String.class);
+	}
+
+ 	// read all book copies by bookCopiesKey
+ 	@GetMapping("/lms/public/library/branches/branch/{branchId}/bookCopies/bookcopy/{bookId}")
+ 	public ResponseEntity<String> copy1(@PathVariable Long branchId,@PathVariable Long bookId) throws SQLException {
+		return restTemplate.getForEntity("http://LIBRARY-SERVICE/lms/public/library/branches/branch/"+branchId+"/bookCopies/bookcopy/"+bookId, String.class);
+	}
+
+
+ 	// update number of book copies
+ 	@PutMapping("/lms/public/library/branches/branch/{branchId}/bookCopies/bookcopy/{bookId}")
+ 	public ResponseEntity<String> copy2(@PathVariable Long branchId,@PathVariable Long bookId) throws SQLException {
+		return restTemplate.getForEntity("http://LIBRARY-SERVICE/lms/public/library/branches/branch/"+branchId+"/bookCopies/bookcopy/"+bookId, String.class);
+	}
+
+    
+    //------------Requests for admin-----------------------
+    @GetMapping(path="/lms/public/admin/authors}")
+	public ResponseEntity<String> author() throws SQLException {
+		return restTemplate.getForEntity("http://ADMIN-SERVICE/lms/public/admin/authors", String.class);
+	}
 }
